49cdb4fffc0d13b44bfbe3ebe6b9c7e235f19a36
sonikunj <sonikunj@sheridancollege.ca>
2021-04-24 00:07:04 -0400

    First Commit For Console App

---
+++ A1KunjSoni/HockeyPlayer.cs
@@ -0,0 +1,55 @@
+﻿/*
+Name: Soni Kunj Mayurkumar
+Student ID: 991591881
+Date of Submission: 17 February 2021
+Topic: Assignment 1
+Guided by: Gursharan Singh Tatla
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A1KunjSoni
+{
+    //Class HockeyPlayer is innheriting from Player class
+    class HockeyPlayer : Player
+    {
+        //fields and getter & setter using property
+        private int _assists;
+
+        public int Assists
+        {
+            get { return _assists; }
+            set { _assists = value; }
+        }
+
+        private int _goals;
+
+        public int Goals
+        {
+            get { return _goals; }
+            set { _goals = value; }
+        }
+
+        //parametrized constructor
+        public HockeyPlayer(PlayerType playerType, int playerId, string playerName, string teamName, int gamesPlayed, int assists, int goals) : base(playerType, playerId,  playerName,  teamName,  gamesPlayed)
+        {
+            Assists = assists;
+            Goals = goals;
+        }
+
+        //Points method
+        public override int Points()
+        {
+            return Assists + (2 * Goals);
+        }
+        
+        //ToString method
+        public override string ToString()
+        {
+            return base.ToString() + $"{Assists, 20} {Goals, 20} {Points(),20}";
+        }
+
+    }
+}
